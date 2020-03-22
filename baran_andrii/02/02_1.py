d3614047cca6123b35e27434dd26ddeafc05fec1
Andrew Baran <abiandreb@gmail.com>
2020-03-22 15:37:32 +0200

    Modified homework files 1, 2. Added additional methods

--- baran_andrii/02/02_1.py
+++ baran_andrii/02/02_1.py
@@ -1,3 +1,5 @@
+# method 1
+
 my_str = input('Type string: ')
 my_o = []
 my_x = []
@@ -15,5 +17,42 @@ for i in my_str:
 if len(my_o) == len(my_x):
     print('True')
 
+else:
+    print('False')
+
+# method 2
+
+my_str = input('Type string: ')
+
+print('Will return "True" if amount of O and X is equal even if amount is 0. '
+      'In other cases will return "False" Not case sensitive.\n')
+temp = []
+for i in my_str:
+    if i == 'O' or i == 'o':
+        temp.append(i)
+
+    elif i == 'X' or i == 'x':
+        temp.append(i)
+
+if not len(temp):
+    print('True')
+
+else:
+    print('False')
+
+# method 3
+
+my_str = input('Type string: ')
+
+print('Will return "True" if amount of O and X is equal even if amount is 0. '
+      'In other cases will return "False" Not case sensitive.\n')
+ls = list(my_str)
+test_ls = ['O', 'o', 'X', 'x']
+counter = 0
+for i in ls:
+    if i in test_ls:
+        counter += 1
+if not counter % 2:
+    print('True')
 else:
     print('False')
--- baran_andrii/02/02_2.py
+++ baran_andrii/02/02_2.py
@@ -16,3 +16,15 @@ new_str = ''
 for i in range(len(my_str)-1, -1,  -1):
     new_str += my_str[i]
 print(new_str)
+
+# Method 3
+
+print('METHOD 3')
+
+my_str = 'Hello'
+my_ls = list(my_str)
+res_str = ''
+for i in range(len(my_str)-1, -1, -1):
+    res_str += my_ls[i]
+
+print(str(res_str))
