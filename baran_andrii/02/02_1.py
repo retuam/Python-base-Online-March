d59f1349beffc9767c65efb8291f313cb2062911
Andrew Baran <abiandreb@gmail.com>
2020-03-23 19:40:36 +0200

    deleted 2 methods on 01_1 file

--- baran_andrii/02/02_1.py
+++ baran_andrii/02/02_1.py
@@ -20,39 +20,3 @@ if len(my_o) == len(my_x):
 else:
     print('False')
 
-# method 2
-
-my_str = input('Type string: ')
-
-print('Will return "True" if amount of O and X is equal even if amount is 0. '
-      'In other cases will return "False" Not case sensitive.\n')
-temp = []
-for i in my_str:
-    if i == 'O' or i == 'o':
-        temp.append(i)
-
-    elif i == 'X' or i == 'x':
-        temp.append(i)
-
-if not len(temp):
-    print('True')
-
-else:
-    print('False')
-
-# method 3
-
-my_str = input('Type string: ')
-
-print('Will return "True" if amount of O and X is equal even if amount is 0. '
-      'In other cases will return "False" Not case sensitive.\n')
-ls = list(my_str)
-test_ls = ['O', 'o', 'X', 'x']
-counter = 0
-for i in ls:
-    if i in test_ls:
-        counter += 1
-if not counter % 2:
-    print('True')
-else:
-    print('False')
